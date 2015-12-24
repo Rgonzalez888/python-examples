28d3745988bb34765f90597a359329df676ed151
Rgonzalez888 <rgonzalez888@gmail.com>
2015-12-23 22:34:08 -0800

    added Template.py to library

---
+++ Template.py
@@ -0,0 +1,10 @@
+"""
+Script Name         : hello_world.py
+Author              : Rafael Gonzalez
+Created             : 12/23/2015
+Last Modified       : 01/02/2016
+Version             : 1.0
+Description         : Program to print 'hello world'
+Modifications       :
+
+"""
